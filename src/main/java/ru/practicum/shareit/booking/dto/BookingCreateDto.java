082ddefefad6fd534c8906b8dc7b2df50fafa4af
Diana Reshetnikova <i@dharchenko.ru>
2025-12-11 22:46:10 +0300

    fix-1

--- src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
@@ -2,12 +2,10 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 
 @Data
-@EqualsAndHashCode(of = "id")
 @Builder
 public class BookingCreateDto {
     private Long itemId;
